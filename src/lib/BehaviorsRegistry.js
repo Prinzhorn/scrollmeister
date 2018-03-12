563a3ca68c436212ee080c35ea22a3750246c921
Prinzhorn <alexander@prinzhorn.it>
2018-03-12 18:37:27 +0100

    keep track of the correct order for behaviors

--- src/lib/BehaviorsRegistry.js
+++ src/lib/BehaviorsRegistry.js
@@ -3,6 +3,7 @@ const lowerCaseAndDashRegex = /^[a-z-]+$/;
 export default class BehaviorsRegistry {
 	constructor() {
 		this._behaviors = {};
+		this._order = [];
 	}
 
 	add(classDefinition) {
@@ -18,7 +19,31 @@ export default class BehaviorsRegistry {
 			);
 		}
 
+		let dependencies = classDefinition.dependencies;
+
+		for (let i = 0; i < dependencies.length; i++) {
+			let dependency = dependencies[i];
+
+			if (dependency.charAt(0) === '^') {
+				dependency = dependency.slice(1);
+			}
+
+			if (!this._behaviors.hasOwnProperty(dependency)) {
+				throw new Error(
+					`You are trying to define a "${name}" behavior that depends on "${dependency}", which is not defined. Make sure to define behaviors in the correct order.`
+				);
+			}
+		}
+
 		this._behaviors[name] = classDefinition;
+
+		//This is an insanely clever "hack".
+		//Every behavior can have dependencies, so behaviors are like a graph.
+		//We could sort/linearize this graph to initialize behaviors in the correct order.
+		//However, we simply assume they are _defined_ in the correct order and track this here.
+		//This totally makes sense and we even throw when defining a behavior that declares
+		//a dependency that does not exist (yet). So the order is guaranteed to be correct.
+		this._order.push(name);
 	}
 
 	has(name) {
@@ -31,5 +56,9 @@ export default class BehaviorsRegistry {
 
 	getNames() {
 		return Object.keys(this._behaviors);
+	}
+
+	getOrder() {
+		return this._order.slice();
 	}
 }
