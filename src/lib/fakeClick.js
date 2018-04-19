f5e34fecc2a437cceda637fbd3c832235962abfd
Prinzhorn <alexander@prinzhorn.it>
2018-04-19 10:03:45 +0200

    focus all faked clicked elements

--- src/lib/fakeClick.js
+++ src/lib/fakeClick.js
@@ -25,27 +25,30 @@ export default {
 		const element = initialTouchElement;
 
 		//Check if it was more like a tap (moved less than 7px).
-		if (distance2 < 49 && element.tagName === 'A') {
+		if (distance2 < 49) {
 			//It was a tap, click the element.
-			let clickEvent = document.createEvent('MouseEvents');
-			clickEvent.initMouseEvent(
-				'click',
-				true,
-				true,
-				e.view,
-				1,
-				touch.screenX,
-				touch.screenY,
-				touch.clientX,
-				touch.clientY,
-				e.ctrlKey,
-				e.altKey,
-				e.shiftKey,
-				e.metaKey,
-				0,
-				null
-			);
-			element.dispatchEvent(clickEvent);
+			if (element.tagName === 'A') {
+				let clickEvent = document.createEvent('MouseEvents');
+				clickEvent.initMouseEvent(
+					'click',
+					true,
+					true,
+					e.view,
+					1,
+					touch.screenX,
+					touch.screenY,
+					touch.clientX,
+					touch.clientY,
+					e.ctrlKey,
+					e.altKey,
+					e.shiftKey,
+					e.metaKey,
+					0,
+					null
+				);
+				element.dispatchEvent(clickEvent);
+			}
+
 			element.focus();
 		}
 	}
